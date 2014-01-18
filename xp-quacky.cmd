c2b7224b46e9c7c04bd9977435115b2e444f4697
Xav Paice <xavpaice@gmail.com>
2014-01-18 15:17:30 +1300

    initial

---
+++ xp-quacky.cmd
@@ -0,0 +1,141 @@
+# Pin name action command file
+
+# Start of element Q1
+ChangePinName(Q1, 2, 2)
+ChangePinName(Q1, 1, 1)
+ChangePinName(Q1, 3, 3)
+
+# Start of element C8
+ChangePinName(C8, 2, -)
+ChangePinName(C8, 1, +)
+
+# Start of element C6
+ChangePinName(C6, 2, -)
+ChangePinName(C6, 1, +)
+
+# Start of element R12
+ChangePinName(R12, 1, 1)
+ChangePinName(R12, 2, 2)
+
+# Start of element R15
+ChangePinName(R15, 1, 1)
+ChangePinName(R15, 2, 2)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element CONN2
+ChangePinName(CONN2, 2, 2)
+ChangePinName(CONN2, 1, 1)
+
+# Start of element CONN1
+ChangePinName(CONN1, 2, 2)
+ChangePinName(CONN1, 1, 1)
+
+# Start of element S1
+ChangePinName(S1, 3, 3)
+ChangePinName(S1, 1, 1)
+ChangePinName(S1, 2, 2)
+ChangePinName(S1, 6, 6)
+ChangePinName(S1, 4, 4)
+ChangePinName(S1, 5, 5)
+
+# Start of element D2
+ChangePinName(D2, 2, 1)
+ChangePinName(D2, 1, 2)
+
+# Start of element D1
+ChangePinName(D1, 2, 1)
+ChangePinName(D1, 1, 2)
+
+# Start of element R14
+ChangePinName(R14, 1, 1)
+ChangePinName(R14, 3, 3)
+ChangePinName(R14, 2, 2)
+
+# Start of element R16
+ChangePinName(R16, 1, 1)
+ChangePinName(R16, 3, 3)
+ChangePinName(R16, 2, 2)
+
+# Start of element R13
+ChangePinName(R13, 1, 1)
+ChangePinName(R13, 3, 3)
+ChangePinName(R13, 2, 2)
+
+# Start of element R8
+ChangePinName(R8, 1, 1)
+ChangePinName(R8, 2, 2)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element R5
+ChangePinName(R5, 1, 1)
+ChangePinName(R5, 2, 2)
+
+# Start of element R9
+ChangePinName(R9, 1, 1)
+ChangePinName(R9, 2, 2)
+
+# Start of element R6
+ChangePinName(R6, 1, 1)
+ChangePinName(R6, 2, 2)
+
+# Start of element R7
+ChangePinName(R7, 1, 1)
+ChangePinName(R7, 2, 2)
+
+# Start of element R11
+ChangePinName(R11, 1, 1)
+ChangePinName(R11, 2, 2)
+
+# Start of element R10
+ChangePinName(R10, 1, 1)
+ChangePinName(R10, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element C7
+ChangePinName(C7, 2, 2)
+ChangePinName(C7, 1, 1)
+
+# Start of element C5
+ChangePinName(C5, 2, 2)
+ChangePinName(C5, 1, 1)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element U1
+ChangePinName(U1, 8, 8)
+ChangePinName(U1, 4, 4)
+ChangePinName(U1, 7, 7)
+ChangePinName(U1, 6, 6)
+ChangePinName(U1, 5, 5)
+ChangePinName(U1, 8, 8)
+ChangePinName(U1, 4, 4)
+ChangePinName(U1, 1, 1)
+ChangePinName(U1, 2, 2)
+ChangePinName(U1, 3, 3)
